fb9c2ba80dd7d288096526ab709df01c7958fc87
Michael Moss <realmike33@gmail.com>
2015-11-10 18:45:17 -0800

    Initial Commit

---
+++ Meetup/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Meetup
+//
+//  Created by Michael Moss on 11/10/15.
+//  Copyright © 2015 Mike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
